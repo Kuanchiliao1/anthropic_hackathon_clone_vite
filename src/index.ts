8dad92f9a000a14f45274d503ce0715c4cd22968
kuanchiliao1 <kuanchiliao@gmail.com>
2023-08-01 20:33:54 -0700

    Fix type errors

--- src/index.ts
+++ src/index.ts
@@ -2,48 +2,21 @@ import "./style.css";
 
 export { sum } from "./sum";
 
-import Anthropic from "@anthropic-ai/sdk";
-
-import { ChatAnthropic } from "langchain/chat_models/anthropic";
-import { HumanMessage } from "langchain/schema";
-import { getResponse, promptWrapper } from "./api";
-
-// const chat = new ChatAnthropic({
-//   temperature: 0.9,
-//   anthropicApiKey:
-//     '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', // In Node.js defaults to process.env.ANTHROPIC_API_KEY
-// });
-
-// const response = await chat.call([
-//   new HumanMessage({
-//     text: 'Hello, how',
-//   }),
-// ]);
-
-// console.log(response)
-
-// console.log('logging response');
-// console.log()
-// fetch('/.netlify/functions/getResponse.js')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-// console.log('still logging...')
-
-const globalAIObject = {
-  name: "AI",
-  stuff: "stuf",
-};
+// import Anthropic from "@anthropic-ai/sdk";
+// import { ChatAnthropic } from "langchain/chat_models/anthropic";
+// import { HumanMessage } from "langchain/schema";
+// import { getResponse, promptWrapper } from "./api";
 
 const classesFadeOut: string[] =
   "transition-opacity ease-in duration-700 opacity-0".split(" ");
 
-const classesFadeIn =
-  "transition-opacity ease-out duration-700 opacity-100".split(" ");
+// const classesFadeIn =
+//   "transition-opacity ease-out duration-700 opacity-100".split(" ");
 
-const localErrorLog =
-  JSON.parse(localStorage.getItem("errorLog")) || "localErrorLog";
-const localMessageHistory =
-  JSON.parse(localStorage.getItem("messageHistory")) || "localMessageHistory";
+const localErrorLog = JSON.parse(localStorage.getItem("errorLog") || "[]");
+const localMessageHistory = JSON.parse(
+  localStorage.getItem("messageHistory") || "[]"
+);
 
 console.log("page path:", window.location.pathname);
 
@@ -58,13 +31,15 @@ console.log({ localErrorLog, localMessageHistory });
 if (window.location.pathname === "/dist/" || window.location.pathname === "/") {
   console.log("main page");
   const link = document.querySelector("a");
-  link.addEventListener("click", event => {
-    event?.preventDefault();
-    document.body.classList.add(...classesFadeOut);
-    setTimeout(() => {
-      document.location.href = "/mainquests.html";
-    }, 700);
-  });
+  if (link) {
+    link.addEventListener("click", event => {
+      event?.preventDefault();
+      document.body.classList.add(...classesFadeOut);
+      setTimeout(() => {
+        document.location.href = "/mainquests.html";
+      }, 700);
+    });
+  }
 } else if (
   window.location.pathname === "/dist/mainquests.html" ||
   window.location.pathname === "/mainquests.html"
@@ -91,17 +66,22 @@ if (window.location.pathname === "/dist/" || window.location.pathname === "/") {
   });
 
   const btn = document.querySelector(".test-retrieval-btn");
-  btn.addEventListener("click", async () => {
-    console.log("btn clicked");
-    const response = await fetch("/.netlify/functions/testFunction").then(
-      res => res.json
-    );
+  if (btn) {
+    btn.addEventListener("click", async () => {
+      console.log("btn clicked");
+      const response = await fetch("/.netlify/functions/testFunction").then(
+        res => res.json
+      );
 
-    document.title = response;
-    console.log(response);
-  });
-  // Fade out animation!
-  // document.body.classList.add(...classes);
+      if (typeof response === "string") {
+        document.title = response;
+      }
+
+      console.log(response);
+    });
+    // Fade out animation!
+    // document.body.classList.add(...classes);
+  }
 }
 
 // const prompt = 'Why is the sky blue?';
