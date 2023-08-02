46fbbcf595cd6a1cf55774683b4c1ceef0cc314b
kuanchiliao1 <kuanchiliao@gmail.com>
2023-08-01 21:25:18 -0700

    Turn off library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,14 @@
 /// <reference types="vitest" />
 import path from "path";
 import { defineConfig } from "vite";
-import packageJson from "./package.json";
 import tailwindcss from 'tailwindcss';
-
-const getPackageName = () => {
-  return packageJson.name;
-};
-
-const getPackageNameCamelCase = () => {
-  try {
-    return getPackageName().replace(/-./g, (char) => char[1].toUpperCase());
-  } catch (err) {
-    throw new Error("Name property in package.json is missing.");
-  }
-};
-
-const fileName = {
-  es: `${getPackageName()}.mjs`,
-  cjs: `${getPackageName()}.cjs`,
-  iife: `${getPackageName()}.iife.js`,
-};
-
-const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
 module.exports = defineConfig({
   base: "./",
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: getPackageNameCamelCase(),
-      formats,
-      fileName: (format) => fileName[format],
-    },
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'), // Point to your app's entry point HTML
+    }
   },
   css: {
     postcss: {
@@ -40,6 +16,6 @@ module.exports = defineConfig({
     },
   },
   test: {
-
+  
   }
 });
